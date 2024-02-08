8c88d788844d2455ffc46c0e0294b0b452f83752
berrua <berru14a@gmail.com>
2024-02-08 22:24:47 +0300

    Added help and case sensitivity parameters

--- essays/essay2/main.go
+++ essays/essay2/main.go
@@ -12,19 +12,26 @@ import (
 
 func main() {
 	// flags
-	zipFilePath := flag.String("file", "", "Specify the compressed file path")
-	searchText := flag.String("text", "", "Specify texts to search for by separating them with (,)")
-	outputFile := flag.String("output", "", "Specify the path to the output file to save the results")
-	caseSensitive := flag.Bool("case-sensitive", false, "Enable case-sensitive search (default -> false)")
+	zipFilePath := flag.String("file", "", "Specify the compressed file path - required")
+	searchText := flag.String("text", "", "Specify the text to search for or specify multiple texts separated by (,) - required")
+	outputFile := flag.String("output", "", "Specify the path to the output file to save the results - optional")
+	caseSensitive := flag.Bool("case-sensitive", false, "Specify whether the search should be case-sensitive - optional")
+	helpFlag := flag.Bool("help", false, "Help for using the finder tool")
 	flag.Parse()
 
 	// control
-	if *zipFilePath == "" || *searchText == "" || *outputFile == "" {
+	if *helpFlag {
+		fmt.Println("Flags;")
 		flag.PrintDefaults()
 		return
 	}
 
-	// search words
+	if *zipFilePath == "" || *searchText == "" {
+		fmt.Println("Please run the -help command to use the Finder tool.")
+		return
+	}
+
+	// separate words
 	searchKeywords := strings.Split(*searchText, ",")
 
 	// open
@@ -37,14 +44,7 @@ func main() {
 	// results
 	var searchResults []string
 
-	// case sensitivity
-	comparisonFunc := strings.Contains
-	if !*caseSensitive {
-		comparisonFunc = func(s, substr string) bool {
-			return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
-		}
-	}
-
+	//
 	for _, file := range zipFile.File {
 		if file.FileInfo().IsDir() {
 			continue
@@ -62,12 +62,23 @@ func main() {
 		// scan
 		for scanner.Scan() {
 			line := scanner.Text()
+			compareLine := line
+			if !*caseSensitive {
+				compareLine = strings.ToLower(line)
+			}
 
+			//compare words
 			for _, keyword := range searchKeywords {
-				if comparisonFunc(line, keyword) {
+				compareKeyword := keyword
+				if !*caseSensitive {
+					compareKeyword = strings.ToLower(keyword)
+				}
+
+				if strings.Contains(compareLine, compareKeyword) {
 					result := fmt.Sprintf("File: %s\n", file.Name)
 					result += fmt.Sprintf("Line Number: %d\n", lineNumber)
 					result += fmt.Sprintf("Line: %s\n", line)
+					result += "----------\n"
 					searchResults = append(searchResults, result)
 					break
 				}
@@ -84,20 +95,26 @@ func main() {
 
 	// output
 	if len(searchResults) > 0 {
-		output, err := os.Create(*outputFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer output.Close()
-
-		for _, result := range searchResults {
-			_, err := output.WriteString(result + "\n")
+		if *outputFile != "" {
+			output, err := os.Create(*outputFile)
 			if err != nil {
 				log.Fatal(err)
 			}
-		}
+			defer output.Close()
 
-		fmt.Println("Search results were saved to", *outputFile)
+			for _, result := range searchResults {
+				_, err := output.WriteString(result)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			fmt.Println("Search results were saved to", *outputFile)
+		} else {
+			for _, result := range searchResults {
+				fmt.Println(result)
+			}
+		}
 	} else {
 		fmt.Println("The searched text was not found.")
 	}
